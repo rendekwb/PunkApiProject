b3479edfb54b473a4e471d4b47a4d19e6c505262
Willie Rendek <rendekwb@gmail.com>
2018-10-03 21:30:40 -0400

    get axios into project

--- src/SearchForm/SearchForm.js
+++ src/SearchForm/SearchForm.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import axios from 'axios'
 import './SearchForm.css'
 
 class SearchForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
+    this.state = {}
+    this.searchBeer = this.searchBeer.bind(this);
+  }
 
-    }
+  searchBeer() {
+    axios
+    .get("https://api.punkapi.com/v2/beers")
+      .then(response =>
+        console.log(response.data)
+      )
+      .catch(error => console.log("error"));
   }
 
   render() {
@@ -19,7 +28,7 @@ class SearchForm extends React.Component {
             </div>
             <div className="search-form">
               <input className="search-form-input" type="text"/>
-              <button className="search-form-button">Search</button>
+              <button className="search-form-button" onClick={this.searchBeer}>Search</button>
             </div>
           </div>
         </div>
