9bb62f96422b296084e03200ed5b4df3e0726dbb
Willie Rendek <rendekwb@gmail.com>
2018-10-13 16:59:54 -0400

    add build directory

---
+++ build/precache-manifest.e713d6f9b18d0df45c2cfc9c9c265ea6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e72e969b10076f1a7090",
+    "url": "/static/js/main.e72e969b.chunk.js"
+  },
+  {
+    "revision": "da04fd78d3990a26ee54",
+    "url": "/static/js/1.da04fd78.chunk.js"
+  },
+  {
+    "revision": "e72e969b10076f1a7090",
+    "url": "/static/css/main.325ca97a.chunk.css"
+  },
+  {
+    "revision": "6bb14f5087f5b091f36a950904150ce5",
+    "url": "/index.html"
+  }
+];
