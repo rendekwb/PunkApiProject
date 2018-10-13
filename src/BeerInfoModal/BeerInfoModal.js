971162bfd2ae91fb084d86d4db1c86fdea3b764b
Willie Rendek <rendekwb@gmail.com>
2018-10-13 16:36:18 -0400

    add beer info modal files

---
+++ src/BeerInfoModal/BeerInfoModal.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './BeerInfoModal.css'
+
+class BeerInfoModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{"I'm gonna be a modal when I grow up."}</p>
+      </div>
+    )
+  }
+}
+
+export default BeerInfoModal;
